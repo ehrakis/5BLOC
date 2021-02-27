68afd1293e8f69689e243ccf312902ffebc64ce4
NicolasBarbey <223106@supinfo.com>
2021-02-27 19:30:34 +0100

    add tests, add commission

--- back/contracts/HouseTransaction.sol
+++ back/contracts/HouseTransaction.sol
@@ -11,27 +11,36 @@ contract HouseTransaction is HouseBuilder {
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
     
     //TODO implementer comission
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable{
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public payable{
         require (housesToOwner[_tokenId] == msg.sender || houses[_tokenId].onSale == true);
+        
+        payable(_from).transfer((msg.value*(100-commission))/100);
+        
         ownerHousesCount[_to] = ownerHousesCount[_to].add(1);
-        ownerHousesCount[msg.sender] = ownerHousesCount[msg.sender].sub(1);
+        ownerHousesCount[_from] = ownerHousesCount[msg.sender].sub(1);
+        
         housesToOwner[_tokenId] = _to;
+        
         emit Transfer(_from, _to, _tokenId);
     }
     
-    function balanceOf(address _owner) external view returns (uint256) {
+    function balanceOf(address _owner) public view returns (uint256) {
         return ownerHousesCount[_owner];
     }
     
-    function ownerOf(uint256 _tokenId) external view returns (address) {
+    function ownerOf(uint256 _tokenId) public view returns (address) {
         return housesToOwner[_tokenId];
     }
     
-    function setOnSale(uint _houseId, bool _onSale) external onlyOwnerOf(_houseId) {
+    function setOnSale(uint _houseId, bool _onSale) public onlyOwnerOf(_houseId) {
         houses[_houseId].onSale = _onSale;
     }
     
-    function withdraw() external onlyOwner {
+    function getOnSale(uint _houseId) public view returns (bool) {
+        return houses[_houseId].onSale;
+    }
+    
+    function withdraw() public onlyOwner {
         address payable _owner = payable(owner());
         _owner.transfer(address(this).balance);
     }
