663bce67caf8d43c66840c44a0d2fa259ccd1b62
Clément GRIMAUD <clement.grimaud@supinfo.com>
2021-03-02 23:51:54 +0100

    Fix address

--- front/js/address.js
+++ front/js/address.js
@@ -1,2 +1,1 @@
-//var supRealEstateAddress = "0x960deAB47Fc4b4b43F71e5f650c0ABFf61881EEe"; Rinkeby
-var supRealEstateAddress = "0x2Dc4f2C300FB1D21A03dfF3bc602946c8e086460";
+var supRealEstateAddress = "0x960deAB47Fc4b4b43F71e5f650c0ABFf61881EEe";
